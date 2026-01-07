4148b28cae7b1e30422bb0bcaf44e147dfc51ba7
SiddheshKumbhar411 <siddheshkumbhar80@gmail.com>
2026-01-07 22:26:22 +0530

    first message

---
+++ Details.java
@@ -0,0 +1,9 @@
+class Details
+{
+   public static void main(String[] args)
+     {
+         System.out.println("Name:siddhesh");
+	System.out.println("location:pune");
+      }
+}
+ 	
---
+++ VariableTwo.java
@@ -0,0 +1,17 @@
+class VariableTwo
+       {
+         public static void main(String [] args)
+                  {
+                      String name="siddhesh";
+		      char grade='A';
+		      byte age=4;
+		      long phno=8446802853l;
+                      float sal=5000.0f;
+		      System.out.println(name);
+		      System.out.println(grade);
+		      System.out.println(age);
+		      System.out.println(phno);
+	              System.out.println(sal);
+		}
+	
+}
