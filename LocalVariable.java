c63712665c62a83d2f92eeb5ef4825d4fe1c28e5
SiddheshKumbhar411 <siddheshkumbhar80@gmail.com>
2026-01-08 23:24:30 +0530

    variable declaration

---
+++ LocalVariable.java
@@ -0,0 +1,14 @@
+class LocalVariable
+       {
+         public static void main(String[] args)
+           {
+		{
+		 byte a=10;//local variable
+                 System.out.println(a);
+		}
+		{
+		 byte a=100;
+		System.out.println(a);//local variable
+                }
+	    }
+        }
