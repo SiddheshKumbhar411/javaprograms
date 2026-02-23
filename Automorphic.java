c0a5f7b433c1872fc6181f9d0bdc99f08cb2eaff
SiddheshKumbhar411 <siddheshkumbhar80@gmail.com>
2026-02-23 23:58:07 +0530

    message

--- Automorphic.java
+++ Automorphic.java
@@ -6,11 +6,23 @@ class Automorphic
 		System.out.print("Enter the no ");
 		int no=sc.nextInt();
 		int power=1;
-		for(int n=no;n!=0;n/=10){
+		*for(int n=no;n!=0;n/=10){
 			power*=10;
 		}
 		int sq=no*no;
 		if(sq%power==no){System.out.println("it is a automorphic no");}
 		else{System.out.println("it is a not a automorphic no");}
+		
+		/*int ct=0;
+		for(int n=no;n!=0;n/=10){
+			ct++;
+		}
+		while(ct!=0){
+			power*=10;
+			ct--;
+		}
+		int sq=no*no;
+		if(sq%power==no){System.out.println("it is a automorphic no");}
+		else{System.out.println("it is a not a automorphic no");}*/
 	}
 }
