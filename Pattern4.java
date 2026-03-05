8d77373b38c352f91d2e7e66469d75b0a5f46b2f
SiddheshKumbhar411 <siddheshkumbhar80@gmail.com>
2026-03-06 00:10:43 +0530

    pattern Programming

---
+++ Pattern4.java
@@ -0,0 +1,25 @@
+class Pattern4 
+{
+	public static void main(String[] args) 
+	{	int n=5;
+		/*for (int i=0;i<n ;i++ )
+		{
+			for (int j=0;j<n-i ;j++ )
+			{
+				System.out.print("*"+" ");
+			}
+			System.out.println(" "+" ");	
+		}*/
+		int temp=n;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<temp;j++)
+			{
+				System.out.print("*"+" ");	
+			}
+			temp--;
+			System.out.println(" "+" ");
+		}
+		
+	}
+}
