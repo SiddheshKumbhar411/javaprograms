9bcc7eaf8af93c9a180e1dbcdb093ea92d1dd39a
SiddheshKumbhar411 <siddheshkumbhar80@gmail.com>
2026-03-06 23:56:38 +0530

    pattern

---
+++ Pattern7.java
@@ -0,0 +1,20 @@
+class Pattern7
+{
+	public static void main(String[] args) 
+	{	int n=5;
+		for(int i=0;i<n;i++)
+		{
+			for (int j=0;j<n-i-1 ;j++ )
+			{	
+				System.out.print(" ");
+			}
+			for (int s=0;s<=2*i;s++)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+			
+		}
+	}	
+}
