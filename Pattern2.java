7de12286b2ccdf3e3978a7229095f9934ae0f823
SiddheshKumbhar411 <siddheshkumbhar80@gmail.com>
2026-03-13 11:10:42 +0530

    pattern programming

---
+++ Pattern2.java
@@ -0,0 +1,17 @@
+class Pattern2 
+{
+	public static void main(String[] args) 
+	{	int n=20;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==j||i==0||i==n-1||j==n-1||j==0||i+j==n-1||i==n/4&&j==n/2){System.out.print("*"+" ");}
+				else{System.out.print("  ");}
+				
+			}
+			System.out.println();
+		}
+		
+	}
+}
