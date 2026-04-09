ad5a32142f650c311bcfa5c2293e9749554b17af
SiddheshKumbhar411 <siddheshkumbhar80@gmail.com>
2026-04-09 12:56:00 +0530

    message

---
+++ Test14.java
@@ -0,0 +1,15 @@
+class Test14 
+{	int a;
+	public static void main(String[] args) 
+	{
+		a=10;
+		System.out.println(a);
+		sun()
+	}
+	public static void sun()
+	{
+		a=20;
+		System.out.println(a);
+		
+	}
+}
