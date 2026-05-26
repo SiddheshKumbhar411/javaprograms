c3d57f5716a407b770be9ef40d4b902f7461e156
SiddheshKumbhar411 <siddheshkumbhar80@gmail.com>
2026-05-26 13:58:46 +0530

    Created bank Application using oops concept

---
+++ BankOOPs.java
@@ -0,0 +1,71 @@
+class BankOOPs 
+{
+	static String ifsc;
+	static String countryCode;
+	static{
+		//	System.out.println("Hello welcome to Canterburry branch");
+		 System.out.println("██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗");
+        System.out.println("██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝");
+        System.out.println("██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗  ");
+        System.out.println("██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝  ");
+        System.out.println("╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗");
+        System.out.println(" ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝");
+
+        System.out.println();
+        System.out.println(" ████████╗ ██████╗ ");
+        System.out.println(" ╚══██╔══╝██╔═══██╗");
+        System.out.println("    ██║   ██║   ██║");
+        System.out.println("    ██║   ██║   ██║");
+        System.out.println("    ██║   ╚██████╔╝");
+        System.out.println("    ╚═╝    ╚═════╝ ");
+
+        System.out.println();
+        System.out.println(" ██████╗ █████╗ ███╗   ██╗████████╗███████╗██████╗ ██████╗ ██╗   ██╗██████╗ ██╗   ██╗");
+        System.out.println("██╔════╝██╔══██╗████╗  ██║╚══██╔══╝██╔════╝██╔══██╗██╔══██╗██║   ██║██╔══██╗╚██╗ ██╔╝");
+        System.out.println("██║     ███████║██╔██╗ ██║   ██║   █████╗  ██████╔╝██████╔╝██║   ██║██████╔╝ ╚████╔╝ ");
+        System.out.println("██║     ██╔══██║██║╚██╗██║   ██║   ██╔══╝  ██╔══██╗██╔══██╗██║   ██║██╔══██╗  ╚██╔╝  ");
+        System.out.println("╚██████╗██║  ██║██║ ╚████║   ██║   ███████╗██║  ██║██████╔╝╚██████╔╝██║  ██║   ██║   ");
+        System.out.println(" ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ");
+
+
+		ifsc="A101";
+		countryCode="IND";
+	}
+	int accno;
+	double balance;
+	String status;
+	{
+		status="active";
+		System.out.println("Thank you for creating account in our bank.We would love to help you");
+		System.out.println("your account no is "+countryCode+this);
+		System.out.println("you balance is"+balance);
+		System.out.println("your acc status is "+status);
+		
+	}
+	String customerName;
+	BankOOPs(String customerName,double balance)
+	{
+		this.customerName=customerName;
+		this.balance=balance;
+	}
+	void AccDetail()
+	{
+		System.out.println("\n[ Account Details ]");
+        System.out.println("Bank     : " +ifsc);
+        System.out.println("Customer : " + customerName);
+        System.out.println("Acc No   : " + countryCode+this);
+        System.out.println("countrycode: " + countryCode);
+        System.out.println("Balance  : " + balance);
+        System.out.println("Status   : " + status);
+	}
+	
+	public static void main(String[] args) 
+	{
+		//System.out.println("Hello World!");
+		BankOOPs a1=new BankOOPs("john",20_00_000);
+		a1.AccDetail();
+		BankOOPs a2=new BankOOPs("mia",50_00_000);
+		a2.AccDetail();
+	
+	}
+}
