abcf4c2b46e9bfa47c407f7e2128d44eb7080fe7
SiddheshKumbhar411 <siddheshkumbhar80@gmail.com>
2026-05-26 13:05:53 +0530

    oops

--- NonStatic1.java
+++ NonStatic1.java
@@ -1,20 +1,28 @@
 class NonStatic1 
 {
-	static
+	int a;
+	/*static
 	{
 		System.out.println("this is  static block");
-	}
-	{
+	}*/
+	/*{
 		System.out.println("this is non static block");
-	}
+		a=10;
+	}*/
 	NonStatic1()
 	{
+		a=10;
 		System.out.println("this is constructor");
 	}
+	NonStatic1(int a)
+	{
+		System.out.println("this is constructor2"+a+" "+this.a);
+	}
+	
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World!");
-		new NonStatic1();
+		new NonStatic1(20);
 		
 	}
 	
